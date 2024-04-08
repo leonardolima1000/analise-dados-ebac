7cf0f31819359161bef632d7786badfadc739689
leonardolima1000 <leonardolima1000@gmail.com>
2024-04-08 21:40:26 +0000

    Adiciona base, código Python e gráfico

---
+++ gasolina.py
@@ -0,0 +1,16 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Carregar dados do arquivo gasolina.csv
+data = pd.read_csv('gasolina.csv')
+
+# Gerar gráfico de linha
+plt.figure(figsize=(10, 6))
+plt.plot(data['dia'], data['venda'], marker='o', color='blue', linestyle='-')
+plt.title('Preço médio de venda da gasolina em São Paulo - Julho 2021')
+plt.xlabel('Dia')
+plt.ylabel('Preço (R$)')
+plt.grid(True)
+plt.savefig('gasolina.png')
+plt.show()
